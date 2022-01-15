0c4b86193fef638b99823348bfac0ad022d1c109
radeknh <radeknh@users.noreply.github.com>
2022-01-15 19:28:48 +0100

    Update adcread.c

--- adcread.c
+++ adcread.c
@@ -50,6 +50,7 @@ int main(int argc, char *argv[]){
     
     // check flags
     for(int i=1; i<argc; i++){  //start from i=1 because argv[0] contains program name
+        printf("%s", argv[i]);
         if(argv[i] == "-n"){
             conversionNumber = atoi(argv[i+1]);
             i++;
